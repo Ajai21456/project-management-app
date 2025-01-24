62439c01f6dcfa9a1406e462f12140ea3db46d48
Ajai21456 <84832509+Ajai21456@users.noreply.github.com>
2025-01-24 16:22:55 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ const connectDB = require('./config/db');
 const authRoutes = require('./routes/auth');
 const projectRoutes = require('./routes/project');
 const taskRoutes = require('./routes/task');
+const errorHandler = require('./utils/errorHandler');
 
 const app = express();
 
@@ -19,5 +20,7 @@ app.use('/api/auth', authRoutes);
 app.use('/api/projects', projectRoutes);
 app.use('/api/tasks', taskRoutes);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// Error handler
+app.use(errorHandler);
+
+module.exports = app;
